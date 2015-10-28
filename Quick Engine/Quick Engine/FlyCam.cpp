b66b7156acd08c5f77e29097601e049c71911822
JustinWeq <zoomdude111@hotmail.com>
2015-10-27 22:16:17 -0700

    Started porting ol test engine files to new quick engine project.

---
+++ Quick Engine/Quick Engine/FlyCam.cpp
@@ -0,0 +1,185 @@
+#include "FlyCam.h"
+
+namespace JR_FlyCam
+{
+	FlyCam::FlyCam()
+	{
+		m_rotX = m_rotY =
+			m_rotZ = m_x =
+			m_y = m_z = m_dx =
+			m_dy = m_dz = 0;
+	}
+
+	void FlyCam::init(float posX, float posY, float posZ, float rotX, float rotY, float rotZ)
+	{
+		m_rotX = rotX;
+		m_rotY = rotY;
+		m_rotZ = rotZ;
+
+		m_x = posX;
+		m_y = posY;
+		m_z = posZ;
+
+	}
+
+	void FlyCam::updateView()
+	{
+		D3DXVECTOR3 up, position, relativePosition, lookAt;
+		float yaw, pitch, roll;
+		D3DXMATRIX rotationMatrix;
+
+
+		// Setup the vector that points upwards.
+		up.x = 0.0f;
+		up.y = 1.0f;
+		up.z = 0.0f;
+
+		//// Setup the position of the camera in the world.
+		//position.x = -m_x;
+		//position.y = -m_y;
+		//position.z = -m_z;
+
+		////Setup the reative position
+
+		// Setup where the camera is looking by default.
+		lookAt.x = 0.0f;
+		lookAt.y = 0.0f;
+		lookAt.z = 1.0f;
+
+		// Set the yaw (Y axis), pitch (X axis), and roll (Z axis) rotations in radians.
+		pitch = m_rotX * 0.0174532925f;
+		yaw = m_rotY * 0.0174532925f;
+		roll = m_rotZ * 0.0174532925f;
+
+		// Create the rotation matrix from the yaw, pitch, and roll values.
+		D3DXMatrixRotationYawPitchRoll(&rotationMatrix, yaw, pitch, roll);
+
+		// Transform the lookAt and up vector by the rotation matrix so the view is correctly rotated at the origin.
+		D3DXVec3TransformCoord(&lookAt, &lookAt, &rotationMatrix);
+		D3DXVec3TransformCoord(&up, &up, &rotationMatrix);
+
+		//set up the relative position
+		relativePosition.x = m_dx;
+		relativePosition.y = m_dy;
+		relativePosition.z = m_dz;
+
+		//rotate the relative position 
+		D3DXVec3TransformCoord(&relativePosition, &relativePosition, &rotationMatrix);
+
+
+		//adde the new position to the m
+
+		m_x += relativePosition.x;
+		m_y += relativePosition.y;
+		m_z += relativePosition.z;
+
+		position.x = -m_x;
+		position.y = -m_y;
+		position.z = -m_z;
+		// Translate the rotated camera position to the location of the viewer.
+		lookAt = position + lookAt;
+
+		// Finally create the view matrix from the three updated vectors.
+		D3DXMatrixLookAtLH(&m_view, &position, &lookAt, &up);
+	}
+
+	void FlyCam::getViewMatrix(D3DXMATRIX &matrix)
+	{
+		matrix = m_view;
+	}
+
+	float FlyCam::getRotX()
+	{
+		return m_rotX;
+	}
+
+	float FlyCam::getRotY()
+	{
+		return m_rotY;
+	}
+
+	float FlyCam::getRotZ()
+	{
+		return m_rotZ;
+	}
+
+	float FlyCam::getPosX()
+	{
+		return m_x;
+	}
+
+	float FlyCam::getPosY()
+	{
+		return m_y;
+	}
+
+	float FlyCam::getPosZ()
+	{
+		return m_z;
+	}
+
+	void FlyCam::setRotX(float rotX)
+	{
+		m_rotX = rotX;
+		if (m_rotX > 90)
+		{
+			m_rotX = 90;
+		}
+
+		if (m_rotX < -90)
+		{
+			m_rotX = -90;
+		}
+	}
+
+	void FlyCam::setRotY(float rotY)
+	{
+		m_rotY = rotY;
+
+	}
+
+	void FlyCam::setRotZ(float rotZ)
+	{
+		m_rotZ = rotZ;
+	}
+
+	void FlyCam::setPosX(float x)
+	{
+		m_x = x;
+	}
+
+	void FlyCam::setPosY(float y)
+	{
+		m_y = y;
+	}
+
+	void FlyCam::setPosZ(float z)
+	{
+		m_z = z;
+	}
+
+	D3DXMATRIX FlyCam::getViewMatrix()
+	{
+		return m_view;
+	}
+
+	void FlyCam::setView(D3DXMATRIX view)
+	{
+		m_view = view;
+	}
+
+	void FlyCam::setDeltaX(float dx)
+	{
+		m_dx = dx;
+	}
+
+	void FlyCam::setDeltaY(float dy)
+	{
+		m_dy = dy;
+	}
+
+	void FlyCam::setDeltaZ(float dz)
+	{
+		m_dz = dz;
+	}
+}
---
+++ Quick Engine/Quick Engine/FlyCam.h
@@ -0,0 +1,69 @@
+#pragma once
+#include "Input.h"
+#include <D3D11.h>
+#include <D3DX10math.h>
+
+namespace JR_FlyCam
+{
+	//a simple class that is meant for scene navigation
+	class FlyCam
+	{
+	public:
+		FlyCam();
+
+		void init(float posX, float posY, float posZ, float rotX, float rotY, float rotZ);
+
+		void updateView();
+
+		void getViewMatrix(D3DXMATRIX &matrix);
+
+		D3DXMATRIX getViewMatrix();
+
+		float getRotX();
+
+		float getRotY();
+
+		float getRotZ();
+
+		float getPosX();
+
+		float getPosY();
+
+		float getPosZ();
+
+		void setRotX(float x);
+
+		void setRotY(float y);
+
+		void setRotZ(float z);
+
+		void setPosX(float x);
+
+		void setPosY(float y);
+
+		void setPosZ(float z);
+
+		void setDeltaX(float dx);
+
+		void setDeltaY(float dy);
+
+		void setDeltaZ(float dz);
+
+		void setView(D3DXMATRIX view);
+	private:
+		float m_rotX;
+		float m_rotY;
+		float m_rotZ;
+
+		float m_dx;
+		float m_dy;
+		float m_dz;
+
+		float m_x;
+		float m_y;
+		float m_z;
+
+		D3DXMATRIX m_view;
+	};
+}
+
